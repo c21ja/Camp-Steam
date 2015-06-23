da015cc228fff9fd86d8de05e57cc2740329257b
Steam <steam@c02kw224fh51.dalton.org>
2015-06-23 15:02:42 -0400

    Camp STEAM

--- big_data_framework/DataAnalysis.java
+++ big_data_framework/DataAnalysis.java
@@ -3,6 +3,29 @@ public class DataAnalysis{
     public static void main(String [] args){
         ActorDatabase database = new ActorDatabase();
 
+
+
+       /* int j=0;
+       * while(j<database.length()-1){
+       *     int i = 0;
+       *     while(i<database.length()-1){
+       *         
+       *         Actor a1 = database.get(i);
+       *         Actor a2 = database.get(i+1);
+       *         long a1Average = a1.getAverage();
+       *         long a2Average = a2.getAverage();
+       *         
+       *         if(a1Average<a2Average){
+       *             Actor temp = database.get(i);
+                    database.set(i,database.get(i+1));
+                    database.set(i+1,temp);
+                }
+                i= i+1;
+            }
+            j=j+1;
+        }
+
+
         /*
          *
          * ENTER YOUR CODE HERE
